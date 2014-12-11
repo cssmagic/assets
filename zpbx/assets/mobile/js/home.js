7ad166a64b0d59c2af8ebcc6d3ddbab08c91f524
zhangpeng <zhangpeng@baixing.com>
2014-12-11 21:25:48 +0800

    Draft.

---
+++ zpbx/assets/mobile/js/home.js
@@ -0,0 +1,7 @@
+var banner = require('./module/home/banner')
+var catList = require('./module/home/cat-list')
+var browserFav = require('./module/browser-fav')
+
+banner.init()
+catList.init()
+browserFav.init()
---
+++ zpbx/static/mobile/js/home.js
@@ -0,0 +1,22 @@
+var banner = {
+	// `/assets/mobile/js/module/home/banner.js` 模块的打包结果
+	init: function () {
+		//...
+	}
+}
+var catList = {
+	// `/assets/mobile/js/module/home/cat-list.js` 模块的打包结果
+	init: function () {
+		//...
+	}
+}
+var browserFav = {
+	// `/assets/mobile/js/module/browser-fav.js` 模块的打包结果
+	init: function () {
+		//...
+	}
+}
+
+banner.init()
+catList.init()
+browserFav.init()
